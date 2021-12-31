2da5ef51e8cd26a413afa6f75ecedf4c6d641571
Tore Aurstad <tore.aurstad@hemit.no>
2022-01-01 00:35:59 +0100

    More work on module 4

--- DemoApp/app/js/directives.js
+++ DemoApp/app/js/directives.js
@@ -1,2 +1,12 @@
 'use strict';
 
+eventsApp.directive('mySample',function($compile) {
+   return {
+       restrict: 'A',
+       link: function(scope, element, attrs, controller) {
+           debugger
+           var markup = "<input type='text' ng-model='sampleData' />{{sampleData}}<br />";
+           angular.element(element).html($compile(markup)(scope));
+       }
+   };
+});
