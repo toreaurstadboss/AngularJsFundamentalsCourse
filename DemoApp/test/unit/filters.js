0e09d4e7d1fd48b8ea92c25b04204dbb05055385
Tore Aurstad <tore.aurstad@hemit.no>
2022-01-03 00:00:23 +0100

    More on unit testing in AngularJs

---
+++ DemoApp/test/unit/filters.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('durations', function() {
+
+    beforeEach(module('eventsApp'));
+
+    it('should return "Half hour" when given a 1', inject(function(durationsFilter){
+
+        expect(durationsFilter(1)).toEqual('Half hour');
+
+    }));
+
+});
